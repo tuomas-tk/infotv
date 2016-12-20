20e65c0645b3bc40ec645be2fe62754ca0c71e7f
Tuomas <tuomas.karj@outlook.com>
2016-12-20 19:35:17 +0200

    Added routes and functionality for announcements

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./server/routes/index');
 var events = require('./server/routes/events');
+var announcements = require('./server/routes/announcements');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'client/public')));
 
 app.use('/', index);
 app.use('/api/events', events);
+app.use('/api/announcements', announcements);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ server/routes/announcements.js
@@ -0,0 +1,138 @@
+var express = require('express');
+var pg = require('pg');
+var router = express.Router();
+
+/* GET all announcements. */
+router.get('/list', function(req, res) {
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({
+        success: false,
+        data: err
+      });
+    } else {
+      client.query('SELECT * FROM announcements', function(err, result) {
+        done();
+        if (err) {
+          console.error(err);
+          return res.status(500).json({
+            success: false,
+            data: err
+          });
+        } else {
+          res.json({
+            success: true,
+            data: result.rows
+          });
+        }
+      });
+    }
+
+  });
+
+});
+
+
+router.put('/create', (req, res) => {
+
+  console.log(data);
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if (err) {
+      done();
+      console.error(err);
+      return res.status(500).json({
+        success: false, data: err
+      });
+
+    } else {
+      client.query('INSERT INTO announcements(title, content) values($1, $1)', [req.body.title, req.body.content], function(err, result) {
+        done();
+        if (err) {
+          console.error(err);
+          return res.status(500).json({
+            success: false, data: err
+          });
+        } else {
+          return res.json({
+            success: true
+          });
+        }
+      });
+    }
+
+  });
+
+});
+
+router.post('/edit', (req, res) => {
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if (err) {
+      done();
+      console.error(err);
+      return res.status(500).json({success: false, data: err});
+
+    } else {
+      client.query('UPDATE announcements SET title = $1, content = $2 WHERE id = $3', [req.body.title, req.body.content, req.body.id], function(err, result) {
+        done();
+        if (err) {
+          console.error(err);
+          return res.status(500).json({
+            success: false,
+            data: err
+          });
+        } else if (result.rowCount != 1) {
+          return res.status(404).json({
+            success: false,
+            data: 'Announcement not found'
+          });
+        } else {
+          return res.json({
+            success: true
+          });
+        }
+      });
+    }
+
+  });
+
+});
+
+router.delete('/delete', (req, res) => {
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if (err) {
+      done();
+      console.error(err);
+      return res.status(500).json({success: false, data: err});
+
+    } else {
+      client.query('DELETE FROM announcements WHERE id=$1', [req.body.id], function(err, result) {
+        done();
+        if (err) {
+          console.error(err);
+          return res.status(500).json({
+            success: false,
+            data: err
+          });
+        } else if (result.rowCount != 1) {
+          return res.status(404).json({
+            success: false,
+            data: 'Announcement not found'
+          });
+        } else {
+          return res.json({
+            success: true
+          });
+        }
+      });
+    }
+
+  });
+
+});
+
+module.exports = router;

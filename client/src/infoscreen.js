7d9113e1b4b9bbe2ba14f1e97992a7c975a82490
Tuomas <tuomas.karj@outlook.com>
2016-12-29 21:10:49 +0200

    Added error handling to infoscreen page

--- client/src/infoscreen.js
+++ client/src/infoscreen.js
@@ -6,53 +6,5 @@ var vue = new Vue({
   el: 'app',
   components: {
     'app': Infoscreen
-  },
-  data: {
-    data: {
-      events: [],
-      announcements: [],
-      error: ''
-    }
-  },
-  methods: {
-    load: function () {
-      var t = this;
-      this.data.error = '';
-      console.log('loading...');
-
-      fetch('/api/events/list')
-        .then(function(response) {
-          return response.json();
-        }).then(function(json) {
-          if (json.success) {
-            t.data.events = json.data;
-          } else {
-            t.data.error += 'Server error 10: ' + json.data;
-          }
-        }).catch(function(ex) {
-          console.log('parsing failed', ex);
-          t.data.error += 'Server error 11';
-        });
-
-      fetch('/api/announcements/list')
-        .then(function(response) {
-          return response.json();
-        }).then(function(json) {
-          if (json.success) {
-            console.log(t.data.announcements);
-            t.data.announcements = json.data;
-          } else {
-            t.data.error += 'Server error 20: ' + json.data;
-          }
-        }).catch(function(ex) {
-          console.log('parsing failed', ex);
-          t.data.error += 'Server error 21';
-        });
-
-      setTimeout(function() { vue.load(); }, 20000);
-
-    }
   }
 });
-
-vue.load();
